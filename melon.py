4292a91719ca83c1d0a3ac066dd30a4adc417fb2
Matthew Zhai <matthewzhai@matthews-mbp.attlocal.net>
2026-01-05 19:37:18 -0800

    ‘moooo’

---
+++ melon.py
@@ -0,0 +1,34 @@
+from system.lib import minescript
+import time
+import random
+
+minescript.execute("warp garden home")
+
+time.sleep(0.5)
+minescript.player_set_orientation(0,-59.0)
+start_pos = minescript.player_position()
+
+minescript.player_press_attack(True)
+minescript.player_press_forward(True)
+while(True):
+    pos = minescript.player_position()
+    if((pos[2]-start_pos[2])%6 < 2.75):
+        if(pos[2]>=-116.5):
+            minescript.player_press_right(True)
+            minescript.player_press_left(False)
+        else:
+            minescript.player_press_right(False)
+            minescript.player_press_left(True)
+    else:
+        if(pos[2]>=-116.5):
+            minescript.player_press_right(False)
+            minescript.player_press_left(True)
+        else:
+            minescript.player_press_right(True)
+            minescript.player_press_left(False)
+    if((pos[0]>142 or pos[0]<-142) and random.randint(0,5) == 0):
+        time.sleep(random.uniform(0,1))
+    if(pos[2]>-103):
+        time.sleep(2+random.uniform(0,1))
+        minescript.execute("warp garden home")
+        time.sleep(0.5)
---
+++ stop.py
@@ -0,0 +1,9 @@
+from system.lib import minescript
+
+minescript.execute("\suspend 1")
+minescript.player_press_attack(False)
+minescript.player_press_left(False)
+minescript.player_press_right(False)
+minescript.player_press_backward(False)
+minescript.player_press_forward(False)
+minescript.execute("\killjob -1")
